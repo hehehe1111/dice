7a3bb6bd8a3d935f81f3a5ba7c37ee47bf131e19
hemantafpv <87780753+hemantafpv@users.noreply.github.com>
2021-08-23 00:07:17 -0400

    Create h.js

---
+++ h.js
@@ -0,0 +1,8 @@
+function housekeeper (name, age, hasworkpermit, languages){
+    this.name = name;
+    this.age = age;
+    this.hasworkpermit = hasworkpermit;
+    this.languages = languages;
+    this.clean = function(){
+        alert("Cleaning in Progress.....")}
+}
